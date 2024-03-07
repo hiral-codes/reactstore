8e293fddd3f2234c4031b963956b58088e6f268c
Hiral Patel <hiralpatel160302@gmail.com>
2024-03-07 20:45:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Products from "./components/Products";
 import About from "./components/About";
 import Cart from "./components/Cart";
 import Profile from "./components/Profile";
+import Header from "./components/Header";
 function App() {
   return (
     <Router>
+          <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
