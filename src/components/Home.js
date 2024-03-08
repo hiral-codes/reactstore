065e70606e3731257fc4034c41cb26e567e61b9d
Hiral Patel <hiralpatel160302@gmail.com>
2024-03-08 23:31:56 +0530

    done with home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ import { useEffect } from "react";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Bubble from "../assets/Bubble";
 function Home() {
   useEffect(() => {
     AOS.init();
@@ -50,7 +51,7 @@ function Home() {
         <div className="container p-6 mx-auto flex gap-3 md:gap-8 flex-wrap justify-center">
           <div
             data-aos="zoom-in"
-            className="card w-full border-2 md:border-4 border-gray-400 bg-[#151515] rounded p-4 md:w-1/3"
+            className="card w-full border-2 md:border-4 border-gray-400 bg-[#1a1a1a] rounded p-4 md:w-1/3"
           >
             <div className="title font-bold text-xl flex items-center gap-2">
               <div className="w-10 h-10 flex items-center justify-center rounded-full flex-shrink-0 bg-indigo-900">
@@ -66,7 +67,7 @@ function Home() {
           </div>
           <div
             data-aos="zoom-in"
-            className="card w-full border-2 md:border-4 border-gray-400 bg-[#151515] rounded p-4 md:w-1/3"
+            className="card w-full border-2 md:border-4 border-gray-400 bg-[#1a1a1a] rounded p-4 md:w-1/3"
           >
             <div className="title font-bold text-xl flex items-center gap-2">
               <div className="w-10 h-10 flex items-center justify-center rounded-full flex-shrink-0 bg-indigo-900">
@@ -83,7 +84,7 @@ function Home() {
           </div>
           <div
             data-aos="zoom-in"
-            className="card w-full border-2 md:border-4 border-gray-400 bg-[#151515] rounded p-4 md:w-1/3"
+            className="card w-full border-2 md:border-4 border-gray-400 bg-[#1a1a1a] rounded p-4 md:w-1/3"
           >
             <div className="title font-bold text-xl flex items-center gap-2">
               <div className="w-10 h-10 flex items-center justify-center rounded-full flex-shrink-0 bg-indigo-900">
@@ -100,7 +101,7 @@ function Home() {
           </div>
           <div
             data-aos="zoom-in"
-            className="card w-full border-2 md:border-4 border-gray-400 bg-[#151515] rounded p-4 md:w-1/3"
+            className="card w-full border-2 md:border-4 border-gray-400 bg-[#1a1a1a] rounded p-4 md:w-1/3"
           >
             <div className="title font-bold text-xl flex items-center gap-2">
               <div className="w-10 h-10 flex items-center justify-center rounded-full flex-shrink-0 bg-indigo-900">
@@ -116,7 +117,7 @@ function Home() {
           </div>
           <div
             data-aos="flip-left"
-            className="card w-full border-2 md:border-4 border-gray-400 bg-[#151515] rounded p-4 md:w-1/3"
+            className="card w-full border-2 md:border-4 border-gray-400 bg-[#1a1a1a] rounded p-4 md:w-1/3"
           >
             <div className="title font-bold text-xl flex items-center gap-2">
               <div className="w-10 h-10 flex items-center justify-center rounded-full flex-shrink-0 bg-indigo-900">
@@ -140,8 +141,64 @@ function Home() {
         Trusted by over 20,000 customers all over the world
       </p>
       <p className="text-center text-sm opacity-60 px-8 mx-auto pb-4">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur modi sit quod.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur
+        modi sit quod.
       </p>
+      {/* Review */}
+      <div className="review flex justify-center items-center px-8 py-8">
+        <div
+          data-aos="zoom-in"
+          className="w-full h-auto shadow-2xl ring-1 bg-[#1a1a1a] rounded relative md:w-1/2 px-4 pt-10 text-center"
+        >
+          <div className="absolute w-24 h-24 rounded-full bg-gray-400 -top-8 left-8 overflow-hidden">
+            <img
+              src="https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTH55NFVb3wAbFJ_MddzDENtnzdg6HO1FBM23Z9p8GbA8SRbrQyMWvJ_OuQ7exhykySZJLbiMlrlbXF18AMelkiqDYrUwERYfbnIUuH_-KpqzZGacsipuQDDFj0vJNqY2r3pz2vhWwvXg"
+              alt=""
+              className="w-full h-full object-cover object-center"
+            />
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+            />
+          </svg>
+          <p className="text-xl opacity-70 pt-4">
+            " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+            nobis et commodi facilis molestiae odit. "
+          </p>
+          <h1 className="text-xl py-2">Chris Martin</h1>
+          <p className="text-xl pb-6">
+            CEO & CO-Founder <strong className="text-purple-500">@babel</strong>
+          </p>
+        </div>
+      </div>
+
+      {/* Contact Us */}
+      <div className="container flex items-center justify-center py-10 px-10">
+        <div data-aos="zoom-in" className="bg-[#1a1a1a] shadow-xl h-auto w-full p-2 md:w-2/3 md:p-16 rounded relative">
+          <h1 className="text-xl text-center md:text-left">Let's Connect !</h1>
+          <p className="md:w-2/4 py-4 text-center md:text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora,
+            labore?
+          </p>
+          <div className="flex justify-center flex-col gap-4 md:flex-row md:justify-start">
+            <input type="text" className="bg-[#1a1a1a] border px-4 py-2 outline-none rounded" placeholder="Email" />
+            <button className="bg-[#5714FF] px-2 rounded py-1">Subscribe</button>
+          </div>
+          <div className="hidden sm:block sm:absolute sm:top-10 sm:right-10">
+            <Bubble/>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
