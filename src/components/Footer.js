328657fdbc111974ef5983dd1ffdfc6204e73746
Hiral Patel <hiralpatel160302@gmail.com>
2024-03-10 18:15:55 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer>
-        <div className='w-full px-10 text-center py-10 text-base'>
+        <div className='w-full px-10 text-center py-5 text-base'>
             Made with 💙 by <a className='text-[#5092FF] font-semibold' href="https://hiral-codes.github.io/" target='_blank' rel='noreferrer'>hiral_codes</a>
         </div>
     </footer>
