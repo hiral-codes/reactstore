f75257c2a4400cf69db42191987db6ac55babb81
Hiral Patel <hiralpatel160302@gmail.com>
2024-03-10 18:15:03 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
         <div className='w-full px-10 text-center py-10 text-base'>
-            Made with 💙 by <a className='text-[#5092FF] semi-bold' href="https://hiral-codes.github.io/" target='_blank' rel='noreferrer'>@hiral_codes</a>
+            Made with 💙 by <a className='text-[#5092FF] font-semibold' href="https://hiral-codes.github.io/" target='_blank' rel='noreferrer'>@hiral_codes</a>
         </div>
     </footer>
   )
